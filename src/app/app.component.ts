7ac7b771f4b2b99cb097c81b4faf69a6ddda132d
Yuriy Gyerts <gyerts@gmail.com>
2021-08-24 09:30:58 +0300

    Add test component to check communication works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div *ngIf="loading">
+      Loading...
+    </div>
+    <div *ngIf="error">
+      Error :(
+    </div>
+    <div *ngIf="rates">
+      <div *ngFor="let rate of rates">
+        <p>{{ rate.currency }}: {{ rate.rate }}</p>
+      </div>
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'angular-graphql-upload-file';
+export class AppComponent implements OnInit {
+  rates: any[]|null = null;
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+    .watchQuery({
+      query: gql`
+        {
+          rates(currency: "USD") {
+            currency
+            rate
+          }
+        }
+      `,
+    })
+    .valueChanges.subscribe((result: any) => {
+      this.rates = result?.data?.rates;
+      this.loading = result.loading;
+      this.error = result.error;
+    });
+  }
 }
